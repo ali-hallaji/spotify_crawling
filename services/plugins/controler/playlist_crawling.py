cb3acb476a26508f12fd8c8b679abbd9102ce7bc
Ali Hallaji <ali.hallaji1@gmail.com>
2017-02-18 22:43:37 +0330

    Fix playlist

--- services/plugins/controler/playlist_crawling.py
+++ services/plugins/controler/playlist_crawling.py
@@ -120,7 +120,9 @@ class PlayListCrawl:
                 )
                 self.save_to_db(response['playlists'].get('items', []))
 
-                while (response.get('playlists', {}) is not None) and (response['playlists'].get('next', '')):
+                one = 'playlists' in response
+                two = response['playlists'] is not None
+                while one and two and response['playlists'].get('next', ''):
                     if not self.allow_time():
                         return
 
